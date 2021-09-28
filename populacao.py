1aaaa573bc59efb660d16117505173f599477d61
bhunao <bhunao@gmail.com>
2021-09-28 16:52:37 -0300

    refactor:

--- populacao.py
+++ populacao.py
@@ -2,24 +2,28 @@ import requests
 import json
 import pandas as pd
 
-u = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-population%40kapsarc&rows=2885&facet=year&facet=country_name"
+# fetch
+url = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-population%40kapsarc&rows=2885&facet=year&facet=country_name"
 
-r = requests.get(u)
-u = r.text
+r = requests.get(url)
+json_data = r.json()
 
-j = json.loads(u)
 
-dat = {'pais': [],
+model = {'pais': [],
        'populacao': [],
        'year': []}
-for i in j['records']:
-  dat['pais'].append(i['fields']['country_name'])
-  dat['populacao'].append(int(i['fields']['value']))
-  dat['year'].append(i['fields']['year'])
 
-dat = pd.DataFrame(dat).sort_values(["pais", "year"])
-dat = dat.drop_duplicates(subset=["pais"], keep="last")
-dat.reset_index(inplace=True)
-df = dat[['pais', 'populacao', 'year']]
-print(df)
-df.to_csv("populacao.csv", index=False)
+# cleaning
+for i in json_data['records']:
+  model['pais'].append(i['fields']['country_name'])
+  model['populacao'].append(int(i['fields']['value']))
+  model['year'].append(i['fields']['year'])
+
+data = pd.DataFrame(model).sort_values(["pais", "year"])
+data = data.drop_duplicates(subset=["pais"], keep="last")
+data.reset_index(inplace=True)
+df = data[['pais', 'populacao', 'year']]
+
+# save
+df.to_csv("populacao_global.csv", index=False)
+print("finalizado com sucesso!")
